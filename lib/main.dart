c6b33c4248684a1d648478229a31d86af513bb30
osim-johnpang1204 <john.pang@osim.com.sg>
2022-01-26 16:20:00 +0800

    implement tecent push notification for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tpns_flutter_plugin/tpns_flutter_plugin.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,11 +31,60 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final XgFlutterPlugin tpush = XgFlutterPlugin();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    /// 开启DEBUG
+    tpush.setEnableDebug(true);
+
+    /// 添加回调事件
+    tpush.addEventHandler(
+      onRegisteredDeviceToken: (String msg) async {
+        print("flutter onRegisteredDeviceToken: $msg");
+      },
+      onRegisteredDone: (String msg) async {
+        print("flutter onRegisteredDone: $msg");
+      },
+      unRegistered: (String msg) async {
+        print("flutter unRegistered: $msg");
+      },
+      onReceiveNotificationResponse: (Map<String, dynamic> msg) async {
+        print("flutter onReceiveNotificationResponse $msg");
+      },
+      onReceiveMessage: (Map<String, dynamic> msg) async {
+        print("flutter onReceiveMessage $msg");
+      },
+      xgPushDidSetBadge: (String msg) async {
+        print("flutter xgPushDidSetBadge: $msg");
+
+        /// 在此可设置应用角标
+        /// tpush.setAppBadge(0);
+      },
+      xgPushDidBindWithIdentifier: (String msg) async {
+        print("flutter xgPushDidBindWithIdentifier: $msg");
+      },
+      xgPushDidUnbindWithIdentifier: (String msg) async {
+        print("flutter xgPushDidUnbindWithIdentifier: $msg");
+      },
+      xgPushDidUpdatedBindedIdentifier: (String msg) async {
+        print("flutter xgPushDidUpdatedBindedIdentifier: $msg");
+      },
+      xgPushDidClearAllIdentifiers: (String msg) async {
+        print("flutter xgPushDidClearAllIdentifiers: $msg");
+      },
+      xgPushClickAction: (Map<String, dynamic> msg) async {
+        print("flutter xgPushClickAction $msg");
+      },
+    );
+
+    tpush.configureClusterDomainName("tpns.sgp.tencent.com");
+    tpush.startXg("1520011026", "APMPAT8PELL9");
   }
 
   @override
@@ -58,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: null,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
