7a04bf6be00073b722138c4cf115359733364f92
osim-johnpang1204 <john.pang@osim.com.sg>
2022-02-17 14:48:43 +0800

    TPNS on IOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:notification_permissions/notification_permissions.dart';
+import 'package:tpns_flutter_plugin/tpns_flutter_plugin.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(_fcmMessageHandler);
+  // await Firebase.initializeApp();
+  // FirebaseMessaging.onBackgroundMessage(_fcmMessageHandler);
 
   runApp(const MyApp());
 }
 
-Future<void> _fcmMessageHandler(RemoteMessage message) async {
-  print('[John] FCM background:  ${message.notification!.body}');
-}
+// Future<void> _fcmMessageHandler(RemoteMessage message) async {
+//   print('[John] FCM background:  ${message.notification!.body}');
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -40,7 +42,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  late FirebaseMessaging messaging;
+  // late FirebaseMessaging messaging;
 
   String? token;
   String? tecentToken;
@@ -53,38 +55,77 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> initFirebase() async {
-    messaging = FirebaseMessaging.instance;
-
-    messaging.getToken().then((value) {
-      setState(() {
-        token = value;
-        textEditingController.text = token!;
-      });
-
-      print('[John] FCM token $value');
-    });
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage event) {
-      print("[John] FCM message recieved");
-    });
-    FirebaseMessaging.onMessageOpenedApp.listen((message) {
-      print('[John] FCM Message clicked!');
-    });
-
-    FirebaseMessaging.onMessageOpenedApp.listen((event) {
-      print('[John] FCM Opened App!');
-    });
+    // messaging = FirebaseMessaging.instance;
+    //
+    // messaging.getToken().then((value) {
+    //   setState(() {
+    //     token = value;
+    //     textEditingController.text = token!;
+    //   });
+    //
+    //   print('[John] FCM token $value');
+    // });
+    //
+    // FirebaseMessaging.onMessage.listen((RemoteMessage event) {
+    //   print("[John] FCM message recieved");
+    // });
+    // FirebaseMessaging.onMessageOpenedApp.listen((message) {
+    //   print('[John] FCM Message clicked!');
+    // });
+    //
+    // FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    //   print('[John] FCM Opened App!');
+    // });
   }
 
-  // final XgFlutterPlugin tpush = XgFlutterPlugin();
+  final XgFlutterPlugin tpush = XgFlutterPlugin();
 
   Future<void> initTecentPlatformState() async {
     /// 开启DEBUG
-    // tpush.setEnableDebug(true);
-    // tpush.configureClusterDomainName("tpns.sgp.tencent.com");
-    // tpush.startXg("1520011026", "APMPAT8PELL9");
-    // XgFlutterPlugin.xgApi.enableOtherPush();
-    // XgFlutterPlugin.xgApi.regPush();
+    tpush.setEnableDebug(true);
+
+    /// 添加回调事件
+    tpush.addEventHandler(
+      onRegisteredDeviceToken: (String msg) async {
+        print("flutter onRegisteredDeviceToken: $msg");
+      },
+      onRegisteredDone: (String msg) async {
+        print("flutter onRegisteredDone: $msg");
+      },
+      unRegistered: (String msg) async {
+        print("flutter unRegistered: $msg");
+      },
+      onReceiveNotificationResponse: (Map<String, dynamic> msg) async {
+        print("flutter onReceiveNotificationResponse $msg");
+      },
+      onReceiveMessage: (Map<String, dynamic> msg) async {
+        print("flutter onReceiveMessage $msg");
+      },
+      xgPushDidSetBadge: (String msg) async {
+        print("flutter xgPushDidSetBadge: $msg");
+      },
+      xgPushDidBindWithIdentifier: (String msg) async {
+        print("flutter xgPushDidBindWithIdentifier: $msg");
+      },
+      xgPushDidUnbindWithIdentifier: (String msg) async {
+        print("flutter xgPushDidUnbindWithIdentifier: $msg");
+      },
+      xgPushDidUpdatedBindedIdentifier: (String msg) async {
+        print("flutter xgPushDidUpdatedBindedIdentifier: $msg");
+      },
+      xgPushDidClearAllIdentifiers: (String msg) async {
+        print("flutter xgPushDidClearAllIdentifiers: $msg");
+      },
+      xgPushClickAction: (Map<String, dynamic> msg) async {
+        print("flutter xgPushClickAction $msg");
+      },
+    );
+    tpush.configureClusterDomainName("tpns.sgp.tencent.com");
+
+    // tpush.startXg("1620011027", "IBHYCA40RD3U"); //IOS
+    tpush.startXg("1520011026", "APMPAT8PELL9"); //ANDROID
+    XgFlutterPlugin.xgApi.enableOtherPush();
+    XgFlutterPlugin.xgApi.regPush();
   }
 
   TextEditingController textEditingController = TextEditingController();
@@ -101,6 +142,15 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            FlatButton(
+                onPressed: () async {
+                  PermissionStatus permissionStatus =
+                      await NotificationPermissions
+                          .getNotificationPermissionStatus();
+
+                  print('[John] Permission Status $permissionStatus');
+                },
+                child: Text('Request Permission')),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
@@ -124,10 +174,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          FirebaseMessaging.instance.requestPermission();
-
-          // tecentToken = await XgFlutterPlugin.xgApi.getXgToken();
-          // textEditingController2.text = tecentToken ?? '';
+          // FirebaseMessaging.instance.requestPermission();
+          tecentToken = await XgFlutterPlugin.xgApi.getXgToken();
+          textEditingController2.text = tecentToken ?? '';
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
